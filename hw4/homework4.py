520d720e3d10ef31536af3b8214294e98af20612
zhao <apple@bean.local>
2017-12-06 18:49:56 -0500

    finish part1

--- hw4/homework4.py
+++ hw4/homework4.py
@@ -11,8 +11,29 @@ def load_cifar10():
     xtrain, ytrain = train
     xtest, ytest = test
 
+    ytrain_1hot = np.zeros((ytrain.shape[0],10))
 
-    #return xtrain, ytrain_1hot, xtest, ytest_1hot
+    for i in range(ytrain.shape[0]):
+        ytrain_1hot[i][ytrain[i][0]] = 1;
+
+    ytest_1hot = np.zeros((ytest.shape[0],10))
+
+    for i in range(ytest.shape[0]):
+        ytest_1hot[i][ytest[i][0]] = 1;
+
+
+    xtrain_normalize = np.zeros((xtrain.shape[0],xtrain.shape[1],xtrain.shape[2],xtrain.shape[3]))
+    for i in range(xtrain.shape[0]):
+        xtrain_normalize[i] = xtrain[i] / 255.0
+
+    xtest_normalize = np.zeros((xtest.shape[0],xtest.shape[1],xtest.shape[2],xtest.shape[3]))
+    for i in range(xtest.shape[0]):
+        xtest_normalize[i] = xtest[i] / 255.0
+
+
+
+
+    return xtrain_normalize, ytrain_1hot, xtest_normalize, ytest_1hot
 
 
 def build_multilayer_nn():
@@ -46,6 +67,9 @@ def train_binary_classifier():
 
 
 if __name__ == "__main__":
-    a = np.zeros((2,2))
-    print(a.shape)
+    xtrain, ytrain_1hot, xtest, ytest_1hot = load_cifar10()
+
+    print(ytrain_1hot[1:3])
+
+
     # Write any code for testing and evaluation in this main section.
